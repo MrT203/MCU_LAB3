562882348b8d0f554cdc4ece382659dc9d88fc58
ANHTANDEPTRAI\Administrator <tanfctk@gmail.com>
2023-11-14 20:30:46 +0700

    Update source and simulation for lab3

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3c/806b70dff182001e160fc5eb784e6bc7
@@ -0,0 +1,15 @@
+/*
+ * global.c
+ *
+ *  Created on: Nov 13, 2023
+ *      Author: Administrator
+ */
+
+
+#include "global.h"
+
+int status = INIT;
+
+int RED = 5;
+int AMBER = 2;
+int GREEN = 3;
---
+++ traffic_light_project/Core/Inc/fsm_traffic_light.h
@@ -0,0 +1,20 @@
+/*
+ * fsm_traffic_light.h
+ *
+ *  Created on: Nov 14, 2023
+ *      Author: Administrator
+ */
+
+#ifndef INC_FSM_TRAFFIC_LIGHT_H_
+#define INC_FSM_TRAFFIC_LIGHT_H_
+
+#include "global.h"
+#include "button.h"
+#include "main.h"
+#include "display.h"
+#include "software_timer.h"
+
+void fsm_traffic_light();
+
+
+#endif /* INC_FSM_TRAFFIC_LIGHT_H_ */
